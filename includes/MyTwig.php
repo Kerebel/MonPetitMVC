35b95d9fc33ae8de823abb366f7ff9c963c2805b
Bastien KEREBEL <bastien.kerebel@gmail.com.>
2021-11-16 02:45:37 +0100

    FIN TP Partie 2

---
+++ includes/MyTwig.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tools;
+
+abstract class MyTwig {
+    
+    private static function getLoader(){
+        $loader = new \Twig_Loader_Filesystem(PATH_VIEW); // dossier contenant les templates
+        // pas de cache en mode Debug
+        return new \Twig_Environment($loader, array(
+            'cache' => false
+        ));
+    }
+    
+    public static function afficheVue($vue, $params){
+        $twig = self::getLoader();
+        
+        $template = $twig->loadTemplate($vue);
+        echo $template->render($params);
+    }
+}
+
--- src/Controller/GestionClientController.php
+++ src/Controller/GestionClientController.php
@@ -4,20 +4,31 @@ namespace APP\Controller;
 use APP\Model\GestionClientModel;
 use ReflectionClass;
 use \Exception;
+use Tools\MyTwig;
 
 class GestionClientController {
     
     public function chercheUn($params) {
         //appel de la méthode find($id) de la classe Model adequate
         $modele = new GestionClientModel();
-        $id = filter_var(intval($params["id"]), FILTER_VALIDATE_INT);
+        // dans tous les cas on récupère les Ids des clients
+        $ids = $modele->findIds();
+        // on place ces Ids dans le tableau de paramètres que l'on va envoyer à la rue
+        $params['lesId']=$ids;
+        // on teste si l'id du client à chercher a été passé dans l'URL
+        if (array_key_exists('id', $params)) {
+            $id = filter_var(intval($params["id"]), FILTER_VALIDATE_INT);
         $unClient = $modele->find($id);
-        if ($unClient) {
-            $r = new ReflectionClass($this);
-            include_once PATH_VIEW . str_replace('Controller', 'View', $r->getShortName()) . "/unClient.php";
-        } else {
-            throw new Exception("Client " . $id . " inconnu");
+        // on place le client trouvé dans le tableau de paramètres que l'on va envoyer à la vue
+        $params['unClient']=$unClient;
         }
+        $r = new ReflectionClass($this);
+        $vue = str_replace('Controller', 'View', $r->getShortName()) . "/unClient.html.twig";
+        MyTwig::afficheVue($vue, $params);
+        // include_once PATH_VIEW . str_replace('Controller', 'View', $r->getShortName()) . "/unClient.php";
+        //} else {
+        //    throw new Exception("Client " . $id . " inconnu");
+        //}
     }
     
     public function chercheTous(){
@@ -26,10 +37,24 @@ class GestionClientController {
         $clients = $modele->findAll();
         if ($clients){
             $r = new ReflectionClass($this);
-            include_once PATH_VIEW . str_replace('Controller', 'View', $r->getShortName()) . "/plusieursClients.php";
+            $vue = str_replace('Controller', 'View', $r->getShortName()) . "/tousClients.html.twig";
+            MyTwig::afficheVue($vue, array('clients' => $clients));
+            // include_once PATH_VIEW . str_replace('Controller', 'View', $r->getShortName()) . "/plusieursClients.php";
         } else {
             throw new Exception("Aucun client à afficher");
         }
+    }
+    
+    public function creerClient($params){
+        $vue = "GestionClientView\\creerClient.html.twig";
+        MyTwig::afficheVue($vue, array());
+    }
+    
+    public function enregistreClient($params) {
+        // création de l'objet client
+        $client = new Client($params);
+        $modele = new GestionClientModel();
+        $modele->enregistreClient($client);
     }
 }
 
